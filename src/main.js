283e049b02ff888fc613473f20c161a08a380690
liangningcong <liangningcong@gmail.com>
2022-04-01 16:01:45 +0800

    update

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ const data = {
 }
 
 function track(target, prop) {
+  if (!activeEffect) return;
+
   let depsMap = bucket.get(target);
   if (!depsMap) {
     bucket.set(target, (depsMap = new Map()));
@@ -13,25 +15,34 @@ function track(target, prop) {
 
   let deps = depsMap.get(prop);
   if (!deps) {
-    depsMap.set(prop, activeEffect)
+    depsMap.set(prop, (deps = new Set()));
   }
+
+  // 收集与副作用函数相关的依赖
+  activeEffect.deps.push(deps);
+
+  deps.add(activeEffect);
 }
 
 function trigger(target, prop) {
   const depsMap = bucket.get(target);
+  if (!depsMap) return;
 
-  if (depsMap) {
-    const fn = depsMap.get(prop);
+  const effects = depsMap.get(prop);
+  if (!effects) return;
 
-    if (fn) {
-      fn()
+  const effectsToRun = new Set(effects); // 直接遍历 effects 会出现死循环
+  effectsToRun.forEach((fn) => {
+    // 防止在一个副作用函数中同时出现对同一个属性的“读”和“写”操作时出现的无限嵌套问题
+    if (fn !== activeEffect) {
+      fn();
     }
-  }
+  });
 }
 
 const refData = new Proxy(data, {
   get(target, prop) {
-    track(target, prop)
+    track(target, prop);
 
     return target[prop];
   },
@@ -41,21 +52,44 @@ const refData = new Proxy(data, {
     trigger(target, prop);
 
     return true;
+  },
+});
+
+// 把某个副作用函数从与它相关的依赖集合中移除
+function cleanup(effectFn) {
+  const deps = effectFn.deps;
+
+  for (let i = 0; i < deps.length; i++) {
+    deps[i].delete(effectFn);
   }
-})
+
+  effectFn.deps.length = 0;
+}
 
 let activeEffect;
+let effectStack = [];
 function effect(fn) {
-  activeEffect = () => {
-    fn()
-  }
-  activeEffect()
+  const effectFn = () => {
+    cleanup(effectFn);
+
+    effectStack.push(effectFn);
+    activeEffect = effectFn;
+
+    fn();
+
+    effectStack.pop();
+    activeEffect = effectStack[effectStack.length - 1];
+  };
+
+  // 用 deps 收集与该副作用函数相关的依赖
+  effectFn.deps = [];
+
+  effect.stack = effect.stack || [];
+  effectFn();
 }
 
 function sayHello() {
-  console.log(refData.name)
+  refData.age++;
 }
 
-effect(sayHello)
-
-refData.name = 'susie'
+effect(sayHello);
